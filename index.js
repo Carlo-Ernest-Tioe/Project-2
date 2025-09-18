43deaaaf9504f9746f59e56a3ddd62936ab543a4
Carlo-Ernest-Tioe <carlotioe@gmail.com>
2025-09-18 11:06:07 +0700

    Dicee

---
+++ index.js
@@ -0,0 +1,25 @@
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource1 = "dice" + randomNumber1 + ".png";
+
+  document.querySelector(".img1").setAttribute("src", randomImageSource1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+  document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+}
+
+document.querySelector(".roll-button").addEventListener("click", rollDice);
+rollDice();
